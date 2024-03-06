493171170a678baf1a6122766cd1eac44d344480
vuniem131104 <niemhoalongvu@gmail.com>
2024-03-06 16:28:10 +0700

    Completion

--- car_parking_counter.py
+++ car_parking_counter.py
@@ -4,9 +4,9 @@ import numpy as np
 import cvzone
 
 video = cv2.VideoCapture('carPark.mp4')
+out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), video.get(cv2.CAP_PROP_FPS), (int(video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 with open('CarParkPos', 'rb') as f:
     positions = pickle.load(f)
-    # print(len(positions))
     
 def checking_car_parking_space():
     total_parking_available = 0
@@ -19,14 +19,10 @@ def checking_car_parking_space():
             cv2.rectangle(frame, pos, (pos[0] + 107, pos[1] + 42), (0, 255, 0), 4)
         else:
             cv2.rectangle(frame, pos, (pos[0] + 107, pos[1] + 48), (0, 0, 255), 2)
-        # cv2.putText(frame, str(white_pixel_count), (x1 + 3, y1), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0,0,255), 1)
-        # cv2.imshow(str(white_pixel_count), crop_car_parking_space)
     return total_parking_available
 
 while True:
     ret, frame = video.read()
-    if video.get(cv2.CAP_PROP_POS_FRAMES) == video.get(cv2.CAP_PROP_FRAME_COUNT):
-        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
     frameTF = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frameTF = cv2.GaussianBlur(frameTF, (3, 3), 1)
     frameTF = cv2.adaptiveThreshold(frameTF, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C ,cv2.THRESH_BINARY_INV, 25, 16)
@@ -37,11 +33,12 @@ while True:
         total_parking_available = checking_car_parking_space()
         cvzone.putTextRect(frame, f'{total_parking_available}/69 LEFT', (50, 50))
         cv2.imshow('Video', frame)
-        # cv2.waitKey(0)
-        if cv2.waitKey(20) & 0xFF == 27:
+        out.write(frame)
+        if cv2.waitKey(1) & 0xFF == 27:
             break 
     else:
         break
 
 video.release()
+out.release()
 cv2.destroyAllWindows()
